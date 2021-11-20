173629f932991827a5b593fa02ccd622a2bff496
xbelda <xaviblx@gmail.com>
2021-11-20 17:31:22 +0100

    Add hyperparameter optimization

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import pandas as pd
 from pytorch_lightning.loggers import mlflow
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
+import optuna
 
 from sklearn.preprocessing import MinMaxScaler
 from src.dataset import MovielensDataset
@@ -17,11 +18,6 @@ DATA_PATH = "./data/processed/ratings.parquet"
 
 SEED = 42
 TRAIN_VAL_RATIO = 0.8
-
-BATCH_SIZE = 256
-EMBEDDING_DIM = 20
-LEARNING_RATE = 1e-3
-DROPOUT = 0.2
 MAX_EPOCHS = 50
 
 
@@ -51,8 +47,20 @@ def train_val_temporal_split(data, train_val_ratio):
     return train_ratings, val_ratings
 
 
-def main():
-    # Define seed
+def objective(trial: optuna.trial.Trial) -> float:
+    # We optimize the number of layers, hidden units in each layer and dropouts.
+    batch_size = trial.suggest_int("batch_size", 32, 256, log=True)
+    embedding_dim = trial.suggest_int("embedding_dim", 8, 32, log=True)
+    dropout = trial.suggest_float("dropout", 0.05, 0.5)
+    lr = trial.suggest_loguniform("lr", 1e-5, 1e-2)
+
+    hyperparameters = dict(batch_size=batch_size,
+                           embedding_dim=embedding_dim,
+                           dropout=dropout,
+                           lr=lr,
+                           seed=SEED)
+
+    # Set seed
     pl.seed_everything(SEED)
 
     # Load Data
@@ -68,28 +76,47 @@ def main():
     train_dataset = MovielensDataset(data=train_ratings)
     val_dataset = MovielensDataset(data=val_ratings)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE,
-                                  shuffle=True, num_workers=4, pin_memory=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE,
-                                shuffle=False, num_workers=4, pin_memory=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                  shuffle=True, num_workers=8, pin_memory=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size,
+                                shuffle=False, num_workers=8, pin_memory=True)
 
     # Model
     model = LightningCollaborativeFiltering(num_users=train_dataset.num_users,
                                             num_movies=train_dataset.num_movies,
-                                            embedding_dim=EMBEDDING_DIM,
-                                            dropout=DROPOUT,
-                                            lr=LEARNING_RATE,
-                                            batch_size=BATCH_SIZE)
+                                            embedding_dim=embedding_dim,
+                                            dropout=dropout,
+                                            lr=lr)
     # Callbacks
-    early_stopping = pl.callbacks.EarlyStopping(monitor="Loss/Val", min_delta=1e-3, patience=3)
+    early_stopping = pl.callbacks.EarlyStopping(monitor="Loss/Val",
+                                                min_delta=1e-3,
+                                                patience=3)
 
     # Logger
-    logger = pl.loggers.MLFlowLogger()
+    logger = pl.loggers.MLFlowLogger(experiment_name="hyperparameter_tuning",
+                                     run_name=f"trial_{trial.number}")
 
     # Train the model
     trainer = pl.Trainer(max_epochs=MAX_EPOCHS, gpus=1, callbacks=[early_stopping], logger=logger)
+    trainer.logger.log_hyperparams(hyperparameters)
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+
+    return trainer.callback_metrics["Loss/Val"].item()
 
 
 if __name__ == "__main__":
-    main()
+    pruner: optuna.pruners.BasePruner = optuna.pruners.MedianPruner()
+
+    study = optuna.create_study(direction="minimize", pruner=pruner)
+    study.optimize(objective, n_trials=20, timeout=600)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: {}".format(trial.value))
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
