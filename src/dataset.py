a2b028b81eb8c967988b7da5b7f476f04ea51784
xbelda <xaviblx@gmail.com>
2021-11-20 23:08:17 +0100

    Create Datamodule class to manage dataset and dataloaders

--- src/dataset.py
+++ src/dataset.py
@@ -1,35 +1,105 @@
-from typing import Dict
+from typing import Dict, Optional, Tuple
 
+import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset
+import pytorch_lightning as pl
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import Dataset, DataLoader
 
 
 class MovielensDataset(Dataset):
-    def __init__(self, data: pd.DataFrame):
-        self.users = data["UserID"].values
-        self.movies = data["MovieID"].values
-        self.ratings = data["ScaledRating"].values
-
-        # TODO: Move this pre-processing outside the dataset
-        # Convert old to new ids
-        self.u2id = {u_id: new_u_id for new_u_id, u_id in enumerate(set(self.users))}
-        self.m2id = {m_id: new_m_id for new_m_id, m_id in enumerate(set(self.movies))}
-
-        self.num_users = len(self.u2id)
-        self.num_movies = len(self.m2id)
+    def __init__(self, users: np.array, movies: np.array, ratings: np.array):
+        self.users = users
+        self.movies = movies
+        self.ratings = ratings
 
     def __len__(self):
         return len(self.users)
 
     def __getitem__(self, idx: int) -> Dict[str, int]:
-        user_id = self.users[idx]
-        movie_id = self.movies[idx]
-        rating = self.ratings[idx]
+        return {"user": self.users[idx],
+                "movie": self.movies[idx],
+                "rating": self.ratings[idx]}
 
-        # Transform ids
-        new_user_id = self.u2id[user_id]
-        new_movie_id = self.m2id[movie_id]
 
-        return {"user": new_user_id,
-                "movie": new_movie_id,
-                "rating": rating}
+class MovielensDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir: str, batch_size: int, train_val_ratio: float):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.train_test_ratio = train_val_ratio
+
+    def prepare_data(self):
+        data = pd.read_parquet(self.data_dir)
+
+        # Convert old to new ids
+        u2id = {u_id: new_u_id for new_u_id, u_id in enumerate(data["UserID"].unique())}
+        m2id = {m_id: new_m_id for new_m_id, m_id in enumerate(data["MovieID"].unique())}
+
+        self.num_users = len(u2id)
+        self.num_movies = len(m2id)
+
+        data["NewUserID"] = data["UserID"].map(u2id)
+        data["NewMovieID"] = data["MovieID"].map(m2id)
+
+        self.data = data
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        # Train/Val split
+        train_ratings, val_ratings = self._train_val_temporal_split(self.data, self.train_test_ratio)
+
+        # Process data
+        train_ratings, val_ratings = self._process_data(train_ratings, val_ratings)
+
+        # Datasets
+        self.train_dataset = MovielensDataset(users=train_ratings["NewUserID"].values,
+                                              movies=train_ratings["NewMovieID"].values,
+                                              ratings=train_ratings["ScaledRating"].values)
+
+        self.val_dataset = MovielensDataset(users=val_ratings["NewUserID"].values,
+                                            movies=val_ratings["NewMovieID"].values,
+                                            ratings=val_ratings["ScaledRating"].values)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=8
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            pin_memory=True,
+            num_workers=8
+        )
+
+    @staticmethod
+    def _train_val_temporal_split(data, train_val_ratio):
+        """Splits `data` in train/validation by time based on `train_val_ratio`.
+
+        This is made by computing the `train_val_ratio` quantile of the "Timestamp"
+        column, then splitting accordingly."""
+        q_timestamp = data["Timestamp"].quantile(train_val_ratio)
+        train_idx = data["Timestamp"] < q_timestamp
+        val_idx = data["Timestamp"] >= q_timestamp
+
+        train_ratings = data[train_idx].copy()
+        val_ratings = data[val_idx].copy()
+
+        return train_ratings, val_ratings
+
+    @staticmethod
+    def _process_data(train_data: pd.DataFrame, val_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        # Scaling
+        scaler = MinMaxScaler()
+        scaler.fit(train_data[["Rating"]])
+
+        train_data["ScaledRating"] = scaler.transform(train_data[["Rating"]]).flatten()
+        val_data["ScaledRating"] = scaler.transform(val_data[["Rating"]]).flatten()
+
+        return train_data, val_data
--- src/train.py
+++ src/train.py
@@ -4,13 +4,10 @@
 # - [Collaborative Filtering in Pytorch](https://spiyer99.github.io/Recommendation-System-in-Pytorch/)
 from typing import Tuple
 
-import pandas as pd
 from pytorch_lightning.loggers import mlflow
-from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
-from sklearn.preprocessing import MinMaxScaler
-from src.dataset import MovielensDataset
+from src.dataset import MovielensDataModule
 from src.model import LightningCollaborativeFiltering
 
 DATA_PATH = "./data/processed/ratings.parquet"
@@ -25,61 +22,24 @@ DROPOUT = 0.2
 MAX_EPOCHS = 50
 
 
-def process_data(train_data: pd.DataFrame, val_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    # Scaling
-    scaler = MinMaxScaler()
-    scaler.fit(train_data[["Rating"]])
-
-    train_data["ScaledRating"] = scaler.transform(train_data[["Rating"]]).flatten()
-    val_data["ScaledRating"] = scaler.transform(val_data[["Rating"]]).flatten()
-
-    return train_data, val_data
-
-
-def train_val_temporal_split(data, train_val_ratio):
-    """Split the `data` in train/validation by time based on `train_val_ratio`.
-
-    This is made by computing the `train_val_ratio` quantile of the "Timestamp"
-    column, then splitting accordingly."""
-    q_timestamp = data["Timestamp"].quantile(train_val_ratio)
-    train_idx = data["Timestamp"] < q_timestamp
-    val_idx = data["Timestamp"] >= q_timestamp
-
-    train_ratings = data[train_idx].copy()
-    val_ratings = data[val_idx].copy()
-
-    return train_ratings, val_ratings
-
-
 def main():
     # Define seed
     pl.seed_everything(SEED)
 
     # Load Data
-    ratings = pd.read_parquet(DATA_PATH)
+    datamodule = MovielensDataModule(DATA_PATH,
+                                     batch_size=BATCH_SIZE,
+                                     train_val_ratio=TRAIN_VAL_RATIO)
 
-    # Train/Val split
-    train_ratings, val_ratings = train_val_temporal_split(ratings, TRAIN_VAL_RATIO)
-
-    # Process data
-    train_ratings, val_ratings = process_data(train_ratings, val_ratings)
-
-    # Datasets
-    train_dataset = MovielensDataset(data=train_ratings)
-    val_dataset = MovielensDataset(data=val_ratings)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE,
-                                  shuffle=True, num_workers=4, pin_memory=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE,
-                                shuffle=False, num_workers=4, pin_memory=True)
+    # Compute internal parameters
+    datamodule.prepare_data()
 
     # Model
-    model = LightningCollaborativeFiltering(num_users=train_dataset.num_users,
-                                            num_movies=train_dataset.num_movies,
+    model = LightningCollaborativeFiltering(num_users=datamodule.num_users,
+                                            num_movies=datamodule.num_movies,
                                             embedding_dim=EMBEDDING_DIM,
                                             dropout=DROPOUT,
-                                            lr=LEARNING_RATE,
-                                            batch_size=BATCH_SIZE)
+                                            lr=LEARNING_RATE)
     # Callbacks
     early_stopping = pl.callbacks.EarlyStopping(monitor="Loss/Val", min_delta=1e-3, patience=3)
 
@@ -87,8 +47,9 @@ def main():
     logger = pl.loggers.MLFlowLogger()
 
     # Train the model
-    trainer = pl.Trainer(max_epochs=MAX_EPOCHS, gpus=1, callbacks=[early_stopping], logger=logger)
-    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+    trainer = pl.Trainer(max_epochs=MAX_EPOCHS, gpus=1,
+                         callbacks=[early_stopping], logger=logger)
+    trainer.fit(model, datamodule=datamodule)
 
 
 if __name__ == "__main__":
